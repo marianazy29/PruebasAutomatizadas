71c752d5f83e6b3f53e67dd9e23b0e9d1f8c6353
Mariana Zúñiga <mariana.zuniga@ucb.edu.bo>
2025-08-17 22:48:33 -0400

    Proyecto inicila

---
+++ PruebasAutomatizadas/Pruebas de Sistema/ContactoTest.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace PruebasAutomatizadas
+{
+    [TestClass]
+    public sealed class ContactoTest
+    {
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Manage().Window.Maximize();
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        
+    }
+}
---
+++ PruebasAutomatizadas/Pruebas de Sistema/LoginTest.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace PruebasAutomatizadas
+{
+    [TestClass]
+    public sealed class LoginTest
+    {
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Manage().Window.Maximize();
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+    }
+}
